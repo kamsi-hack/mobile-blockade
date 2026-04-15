51224ea0fe235b7d86669abe662bbe2b5b80f05c
kamsi-hack <kamsinwabueze155@gmail.com>
2026-04-15 14:49:24 +0100

    Implement basic player movement and mouse look

---
+++ playercontroller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform cameraTransform;
+
+    float xRotation = 0f;
+
+    void Update()
+    {
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.Translate(move * speed * Time.deltaTime);
+
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
